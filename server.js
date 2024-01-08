e7ca913885f04624c2ae3e17730f48cf5281245c
skrsumit250 <skrsumit250@gmail.com>
2024-01-09 01:15:49 +0530

    change

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT||3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/submit-form', (req, res) => {
+app.post('/form', (req, res) => {
   const { name, email, message } = req.body;
 
   if (!name || !email || !message) {
